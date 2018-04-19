dd7bb0b3df04b03a32525957a52c77b588539904
Aren Chen <yarencheng@gmail.com>
2018-04-18 23:40:23 -0700

    test

--- dummy_supervisor.go
+++ dummy_supervisor.go
@@ -1,39 +1,22 @@
 package cryptotrade
 
 import (
-	"time"
-
 	"github.com/sirupsen/logrus"
 	"github.com/yarencheng/crypto-trade/data"
 )
 
 type DummySupervisor struct {
-	isRun bool
 }
 
 func NewDummySupervisor() *DummySupervisor {
 	var d DummySupervisor
-	d.isRun = false
+
 	return &d
 }
 
 func (dummy *DummySupervisor) Supervise(orders chan data.Order) {
 
-	dummy.isRun = true
-
-	for {
-		select {
-		case order := <-orders:
-			logrus.Infoln("receive order", order)
-		default:
-			if !dummy.isRun {
-				break
-			}
-			time.Sleep(time.Second)
-		}
+	for order := range orders {
+		logrus.Infoln("receive order", order)
 	}
-}
-
-func (dummy *DummySupervisor) Stop() {
-	dummy.isRun = false
 }
