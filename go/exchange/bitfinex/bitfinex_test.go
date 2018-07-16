2956b2984820846f35fabe670b42a7f72cf8e247
Aren Chen <yarencheng@gmail.com>
2018-07-16 10:44:11 -0700

    test

--- go/exchange/bitfinex/bitfinex_test.go
+++ go/exchange/bitfinex/bitfinex_test.go
@@ -2,7 +2,6 @@ package bitfinex
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
 
@@ -31,49 +30,4 @@ func Test_Start(t *testing.T) {
 	assert.NoError(t, err)
 
 	time.Sleep(10 * time.Second)
-}
-
-type I interface {
-	ff()
-}
-
-type A struct {
-	a int
-}
-
-func (a *A) InitA() {
-	fmt.Println("A.InitA()")
-	a.a = 1
-	a.Fn()
-}
-
-func (a *A) Fn() {
-	fmt.Println("A.Fn()")
-	a.a = 2
-}
-
-type B struct {
-	A
-	I
-	a int
-}
-
-func (b *B) Fn() {
-	fmt.Println("B.Fn()")
-	b.a = 222
-}
-
-func Test_Startssss(t *testing.T) {
-	b := &B{}
-	b.InitA()
-
-	fmt.Println(b.a)
-
-	b.Fn()
-
-	fmt.Println(b.a)
-
-	b.ff()
-
-	t.Fail()
 }
