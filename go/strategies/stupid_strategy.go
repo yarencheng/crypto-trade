7941bad4adfbc87a03d3aa9ef4bf8b740b2dfcb2
Aren Chen <yarencheng@gmail.com>
2018-08-02 23:30:23 +0800

    test

--- go/strategies/stupid_strategy.go
+++ go/strategies/stupid_strategy.go
@@ -2,8 +2,11 @@ package strategies
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
+	"github.com/emirpasic/gods/trees/avltree"
+	"github.com/emirpasic/gods/utils"
 	"github.com/yarencheng/crypto-trade/go/entity"
 	"github.com/yarencheng/crypto-trade/go/logger"
 )
@@ -13,11 +16,14 @@ type StupidStrategy struct {
 	wg        sync.WaitGroup
 	InOrders  <-chan entity.OrderBookEvent
 	OutOrders chan<- entity.BuyOrderEvent
+	// exchange > from > to > price > volume
+	priceBook map[entity.Exchange]map[entity.Currency]map[entity.Currency]*avltree.Tree
 }
 
 func NewStupidStrategy() *StupidStrategy {
 	return &StupidStrategy{
-		stop: make(chan int, 1),
+		stop:      make(chan int, 1),
+		priceBook: make(map[entity.Exchange]map[entity.Currency]map[entity.Currency]*avltree.Tree),
 	}
 }
 
@@ -67,6 +73,13 @@ func (this *StupidStrategy) worker() {
 			return
 		case order := <-this.InOrders:
 			logger.Debugf("Receive an order [%v]", order)
+
+			err := this.updatePriceBook(&order)
+			if err != nil {
+				logger.Warnf("Update order [%v] failed. err: [%v]", err)
+				return
+			}
+
 			this.OutOrders <- entity.BuyOrderEvent{
 				Type: entity.None,
 			}
@@ -83,5 +96,39 @@ func (this *StupidStrategy) worker() {
 			// }
 		}
 	}
+}
 
+func (this *StupidStrategy) updatePriceBook(order *entity.OrderBookEvent) error {
+	switch order.Type {
+	case entity.ExchangeRestart:
+		delete(this.priceBook, order.Exchange)
+		return nil
+	case entity.Update:
+	default:
+		return fmt.Errorf("Unknown even type [%v]", order.Type)
+	}
+
+	var ok bool
+
+	var from map[entity.Currency]map[entity.Currency]*avltree.Tree
+	if from, ok = this.priceBook[order.Exchange]; !ok {
+		from = make(map[entity.Currency]map[entity.Currency]*avltree.Tree)
+		this.priceBook[order.Exchange] = from
+	}
+
+	var to map[entity.Currency]*avltree.Tree
+	if to, ok = from[order.From]; !ok {
+		to = make(map[entity.Currency]*avltree.Tree)
+		from[order.From] = to
+	}
+
+	var price *avltree.Tree
+	if price, ok = to[order.To]; !ok {
+		price = avltree.NewWith(utils.Float64Comparator)
+		to[order.To] = price
+	}
+
+	price.Put(order.Price, order.Volume)
+
+	return nil
 }
